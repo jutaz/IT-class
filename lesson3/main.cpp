61faa04f995b91393471699bbd6cef3faf6ea442
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-09-24 09:38:33 +0300

    update to lesson3

--- lesson3/main.cpp
+++ lesson3/main.cpp
@@ -8,20 +8,36 @@
 using namespace std;
 
 vector<int> read_file(char* filename);
+int ord(unsigned char chr);
 
 int main(int argc, char** argv) {
     vector<int> data = read_file("./data.txt");
+    cout << (char) data[0] <<endl;
     return 0;
 }
 
 vector<int> read_file(char* filename) {
     string line;
+    string all;
     vector<int> data;
     ifstream file;
     file.open(filename);
     while(!file.eof()) {
         getline(file, line, '\n');
-        data.push_back(atoi(line));
+        const char* x =line.c_str();
+        unsigned char* tmp;
+        tmp = (unsigned char) x;
+        cout << ord(tmp) <<endl;
+        data.push_back((int) tmp);
     }
     return data;
+}
+
+bool write_file(string data, char* filename) {
+
+}
+
+int ord(unsigned char chr) {
+    int ret = int(chr);
+    return ret;
 }
